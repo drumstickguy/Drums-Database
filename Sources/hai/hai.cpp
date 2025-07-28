ca2b787bd027a5b5170169e7d4fb7b471a098afb
64 <jamieleetaggart643@gmail.com>
2025-07-28 01:16:53 -0400

    Truly the most "original" thing I ever done yet
    Visual Studio 1998 test
    Man, dsp files and dsw files are a-

---
+++ Sources/hai/hai.cpp
@@ -0,0 +1,16 @@
+// hai.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+
+int APIENTRY WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR     lpCmdLine,
+                     int       nCmdShow)
+{
+	MessageBoxA(NULL, "Yo, Visual Studio 1998!", "Hello from a old compiler.", MB_ICONERROR | MB_OK);
+	return 0;
+}
+
+
+
